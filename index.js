e2b574a863c2fc9d1148658f6951393a2db0f404
Markus Lautenbach <markus.lautenbach@gmail.com>
2017-05-07 12:27:01 +0200

    use example index.js with port from env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+var express = require('express');
+var app = express();
 
-var express = require('express')
-var app = express()
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
-app.listen(443, function () {
-  console.log('listening on port 443')
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
